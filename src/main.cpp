5d1dc1859610567446767651f566f16001acd4a3
Mark Bradley <markbradley27@gmail.com>
2022-07-28 17:54:08 -0700

    Reads pm25 values from PMS5003 AQI sensor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "Arduino.h"
 #include "DHTesp.h"
 #include "DS3231.h"
+#include "PMserial.h"
+#include "SoftwareSerial.h" // Required for PMserial.
 
 #include "ring_buffer.h"
 #include "util.h"
@@ -13,18 +15,23 @@ DHTesp dht;
 RingBuffer<float> temp_c_values(10 * 60 / READ_SENSORS_INTERVAL_S);
 RingBuffer<float> humidity_values(10 * 60 / READ_SENSORS_INTERVAL_S);
 
+// PMS5003 AQI sensor
+SerialPM pms(PMSx003, D3, D4);
+RingBuffer<uint16_t> pm25_values(10 * 60 / READ_SENSORS_INTERVAL_S);
+
 // DS3231 RTC
 RTClib rtc;
 
+// Timers
 Timer timer_read_sensors = {seconds(READ_SENSORS_INTERVAL_S)};
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Heyo!");
 
-  Wire.begin();
-
+  Wire.begin(); // Required for RTC.
   dht.setup(DHT_DATA_PIN, DHTesp::DHT22);
+  pms.init();
 }
 
 void ReadAllSensors() {
@@ -46,6 +53,11 @@ void ReadAllSensors() {
   if (!isnan(humidity)) {
     humidity_values.Insert(humidity, millis());
     Serial.println("Humidity: " + String(humidity_values.Latest().value) + "%");
+  }
+
+  if (pms.read() == OK) {
+    pm25_values.Insert(pms.pm25, millis());
+    Serial.println("PM2.5: " + String(pm25_values.Latest().value));
   }
 }
 
